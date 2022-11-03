4ff2a48c44b6158f0e0eb5ea77e73383c66e4baf
SeanHLam <91505213+SeanHLam@users.noreply.github.com>
2022-11-03 01:01:54 -0700

    added db

--- index.js
+++ index.js
@@ -1,11 +1,32 @@
 const express = require("express")
+const sqlite3 = require('sqlite3').verbose();
+const db = new sqlite3.Database('./db');
+
+db.run('CREATE TABLE IF NOT EXISTS users(id INTEGER PRIMARY KEY AUTOINCREMENT, name varchar(15),streak int)')
 
 const app = express()
 
 app.get('/',(req,res)=>{
-    console.log(req.query)
-    console.log("hello")
+    // db.each("SELECT * from users", (err, row)=>{
+    //     res.send(row)
+        
+    // })
+    res.end()
+})  
+
+app.get('/add-user',(req,res)=>{
+    const name = req.query.name
+    const streak = req.query.streak
+    db.run(`INSERT INTO users (name,streak) values (?,?)`, [name, streak])
+
+    db.each("SELECT * from users", (err, row)=>{
+        console.log(row)
+    })
     res.end()
 })
 
-app.listen(3000)
+app.get('/load-scores',(req,res)=>{
+    res.json(db)
+})  
+
+app.listen(3001)
