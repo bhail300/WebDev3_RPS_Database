68bc0de49bf4ef8a83cf23e44c43435e19ed49c0
SeanHLam <91505213+SeanHLam@users.noreply.github.com>
2022-11-09 11:20:03 -0800

    added database order

--- index.js
+++ index.js
@@ -1,33 +1,43 @@
 const express = require("express")
+const cors = require('cors')
 const sqlite3 = require('sqlite3').verbose();
+
 const db = new sqlite3.Database('./db');
 
-db.run('CREATE TABLE IF NOT EXISTS users(id INTEGER PRIMARY KEY AUTOINCREMENT, name varchar(15),streak int)')
+
+
+db.run('CREATE TABLE IF NOT EXISTS users(id INTEGER PRIMARY KEY AUTOINCREMENT, name varchar(15), wins int)')
 
 const app = express()
+app.use(cors())
+
+var user=""
+
+var scores=[]
 
 app.get('/',(req,res)=>{
-    // db.each("SELECT * from users", (err, row)=>{
-    //     res.send(row)
-        
-    // })
     res.end()
-})  
+}) 
+
+
 
 app.get('/add-user',(req,res)=>{
-    const name = req.query.name
-    const streak = req.query.streak
+    user = req.query.name
+    wins = req.query.win
     //update instead
-    db.run(`INSERT INTO users (name,streak) values (?,?)`, [name, streak])
-
-    db.each("SELECT * from users", (err, row)=>{
-        console.log(row)
-    })
+    db.run(`INSERT INTO users (name, wins) values (?, ?)`, [user, wins])
+    db.all("SELECT * from users ORDER BY wins DESC", (err, row)=>{
+            console.log(row)
+        })
     res.end()
 })
 
 app.get('/load-scores',(req,res)=>{
-    res.json(db)
+    db.all("SELECT * from users ORDER BY wins DESC", (err, row)=>{
+        console.log(row)
+        res.json(row)
+    })
+    
 })  
 
 app.listen(3001)
